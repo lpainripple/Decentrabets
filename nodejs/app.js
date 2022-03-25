f28f7545090f33465baff5b694e54ee9e714b475
MarcelToma <marceltoma@gmail.com>
2022-03-25 11:28:07 -0300

    fixed app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,19 +7,11 @@ let configFile = fs.readFileSync(__dirname+'/config.json')
 let dbconfig = JSON.parse(configFile);
 
 const pool = new Pool({
-<<<<<<< HEAD
   user: dbconfig.database.username,
   host: dbconfig.database.host,
   database: dbconfig.database.database,
   password: dbconfig.database.password,
   port: dbconfig.database.port,
-=======
-  user: 'decentrabets',
-  host: 'decentrabets.ddns.net',
-  database: '',
-  password: '',
-  port: 5432,
->>>>>>> 0d4f6f24e5df4c3f72931e114ba7fdfc8cc355ec
 })
 
 pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
