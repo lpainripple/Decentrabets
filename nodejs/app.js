83fb367bab1da65183f25af177a03ead181fe2a2
MarcelToma <marceltoma@gmail.com>
2022-03-25 10:59:34 -0300

    Database connection example

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+'use strict';
+
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: 'decentrabets',
+  host: 'decentrabets.ddns.net',
+  database: 'decentrabets',
+  password: 'q1w2e3',
+  port: 5432,
+})
+
+pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    results.rows.forEach(element => {
+        console.log(element.email)
+    });
+})
