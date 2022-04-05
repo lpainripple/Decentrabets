50808b5c5e6db8490925d5351bd239ce1c145825
lpainripple <lpain@ripple.com>
2022-04-05 09:46:58 -0300

    server.js file

---
+++ Decentrabets creation.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE Decentrabets;
+
+USE Decentrabets;
+
+CREATE TABLE Users (
+    user_name       	varchar(40),
+    pass       			varchar(40) NOT NULL,
+    email				varchar(100),
+    account_address 	varchar(100),
+    balance   			NUMERIC (15, 8),
+    PRIMARY KEY (user_name)
+    ) ;
+    
+INSERT INTO Users (user_name, pass, account_address, balance)
+VALUES ('Tiago', 'asdf', '1029394', 52344.12345678),
+		('Luis', 'asdf', 'aslkdjghlaksdjgh', 123.123),
+		('Anderson', 'asdf', 'oqwiueyroiwqueaskjdhflkasjdhfxzlxcjhvljkxcvxz', 54321.544321),
+        ('Alex', 'asdf', '123', 1.1),
+		('Marcel', 'asdf', '1', 0);
+
+Select * from Users;
+
---
+++ server.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const res = require('express/lib/response');
+const Pool = require("pg").Pool;
+const fs = require("fs");
+const session = require("express-session");
+const flash = require('express-flash');
+const path = require("path");
+
+let configFile = fs.readFileSync(__dirname + "/config.json");
+let dbconfig = JSON.parse(configFile);
+
+//put in the config file
+const pool = new Pool({
+//   user: dbconfig.database.username,
+//   host: dbconfig.database.host,
+//   database: dbconfig.database.database,
+//   password: dbconfig.database.password,
+//   port: dbconfig.database.port,
+    user: "decentrabets",
+    host: "decentrabets.ddns.net",
+    database: "decentrabets",
+    password: "q1w2e3",
+    port: "5432",
+});
+
+app.use(
+    session({
+        //put in the config file
+      secret: "7cfdf87a-2e82-4ec5-90ae-1223452d1373",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+  app.use(express.json());
+  //allow us to access request variables in the post methods
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, "/views/static")));
+
+app.set('view-engine', 'ejs');
+
+//app.use(express.urlencoded({extended: false}));
+
+app.get('/', function(request, response) {
+    response.render('index.ejs', { test: 'test1'});
+});
+
+/********************************** LOGIN ************************************* */
+app.get('/login', function(request, response){
+    response.render('login.ejs');
+});
+
+app.post('/login', function(request, response){
+    let username = request.body.username;
+    let password = request.body.password;
+  
+    if (username && password) {
+      console.log(`Input - username: ${username} / password: ${password}`);
+      console.log(`running query...`);
+      pool.query(
+        "SELECT * FROM users where user_name = $1 and pass = $2",
+        [username, password],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          results.rows.forEach((element) => {
+            console.log(`Found user: ${element.user_name} in the database`);
+          });
+          console.log(results.rowCount);
+          if (results.rowCount == 1) {
+            console.log(`User ${results.rows[0]} logged in...`);
+            //Authentication
+            request.session.loggedin = true;
+            request.session.username = username;
+            console.log("session initialized...");
+            //Redirect to homepage
+            response.redirect("/home");
+          } else {
+            response.send("Incorrect username or password");
+          }
+        }
+      );
+    } else {
+      response.send("Please enter username and password");
+      response.end();
+    }
+});
+/********************************** LOGIN END ************************************* */
+
+
+/********************************** REGISTER ************************************* */
+app.get('/register', function(request, response){
+    response.render('register.ejs');
+});
+
+app.post('/register', async function(request, response){
+    //able to access because of app.use(expres...
+    try {
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+        const username = request.body.username;
+        const email = request.body.email;
+        //const password = hashedPassword;
+        const password = request.body.password;
+
+        pool.query(
+            "insert into USERS (user_name, pass, account_address, email, balance) values ($1, $2, '', $3, 0);",
+            [username, password, email],
+            (error, results) => {
+              if (error) {
+                throw error;
+              }
+            console.log('user added to database:' + results.response);
+              
+            console.log(username);
+            console.log(password);
+            console.log(email);
+            console.log("Finished with no errors...");
+            console.log(`User ${request.body.username} registered.`)
+
+            //Redirect to homepage
+            response.redirect("/login");
+            }
+          );
+
+          //TODO: CREATE WALLET 
+    }
+    catch (exception) {
+        response.redirect('/register');
+        console.log(exception);
+    }
+});
+/********************************** REGISTER END ************************************* */
+
+/********************************** HOME ************************************* */
+
+app.get('/home', function(request, response){
+  response.send(`The user ${request.session.username} is logged in: ${request.session.loggedin}`);
+});
+
+/********************************** HOME END ************************************* */
+
+app.listen(3000);
