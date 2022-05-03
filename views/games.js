f60b93bfb2e324b19407ce65f79d292db613e718
alexkarchermonteiro <alexkarcher@gmail.com>
2022-05-02 23:47:40 -0300

    Update games.js
    Insert game fixtures into database

--- views/games.js
+++ views/games.js
@@ -1,6 +1,6 @@
 
 
-function getFixtures()
+function getFixtures(seasonId)
 {
   if(token == null)
     var token = getToken();
@@ -9,7 +9,8 @@ function getFixtures()
   var currentDate = new Date();
   currentDate.setDate(currentDate.getDate() - 14);
 
-  var address = "https://football.elenasport.io/v2/seasons/4200/fixtures?from=" + currentDate.toISOString().slice(0, 10);
+  //champions league season:4200
+  var address = "https://football.elenasport.io/v2/seasons/${seasonId}/fixtures?from=" + currentDate.toISOString().slice(0, 10);
 
   httpReq.open("GET", address, false);
 
@@ -27,20 +28,20 @@ function getFixtures()
 
     var game = jsonData.data[i];
 
-	gameResults[i] = {};
+  	gameResults[i] = {};
     gameResults[i].date = game.date.slice(0, 16);
     gameResults[i].id = game.id;
-	gameResults[i].idHome = game.idHome;
-	gameResults[i].homeName = game.homeName;
-	gameResults[i].idAway = game.idAway;
-	gameResults[i].awayName = game.awayName;
-	gameResults[i].status = game.status;
-	gameResults[i].seasonName = game.seasonName;
-	gameResults[i].scoreHome = game.team_home_90min_goals + game.team_home_ET_goals;
-	gameResults[i].scoreAway = game.team_away_90min_goals + game.team_away_ET_goals;
- 	gameResults[i].team_home_PEN_goals = game.team_home_PEN_goals;
-	gameResults[i].team_away_PEN_goals = game.team_away_PEN_goals;
-  }
+    gameResults[i].idHome = game.idHome;
+    gameResults[i].homeName = game.homeName;
+    gameResults[i].idAway = game.idAway;
+    gameResults[i].awayName = game.awayName;
+    gameResults[i].status = game.status;
+    gameResults[i].seasonName = game.seasonName;
+    gameResults[i].scoreHome = game.team_home_90min_goals + game.team_home_ET_goals;
+    gameResults[i].scoreAway = game.team_away_90min_goals + game.team_away_ET_goals;
+    gameResults[i].team_home_PEN_goals = game.team_home_PEN_goals;
+    gameResults[i].team_away_PEN_goals = game.team_away_PEN_goals;
+    }
 
   return gameResults;
 }
@@ -67,4 +68,48 @@ function getFixtures()
     function callbackFunction(xmlhttp) 
     {
         //alert(xmlhttp.responseXML);
+    }
+
+    function loadGamestoDatabase(seasonIds, category)
+    {
+      const Pool = require("pg").Pool;
+      const pool = new Pool
+      (
+        {
+          user: "decentrabets",
+          host: "decentrabets.ddns.net",
+          database: "decentrabets",
+          password: "q1w2e3",
+          port: "5432",
+        }
+      );
+      
+      //const columns = await pool.query('SELECT * FROM information_schema.columns WHERE TABLE_NAME = games;')
+
+      for(season in seasonIds)
+      {
+        const games = getFixtures(season);
+
+        for (game in games)
+        {
+          const result = await pool.request()
+            .input('c1', sql.dat, game.date)
+            .input('c2', sql.VarChar(40), game.id)
+            .input('c3', sql.VarChar(40), game.idHome)
+            .input('c4', sql.VarChar(40), game.homeName)
+            .input('c5', sql.VarChar(40), game.idAway)
+            .input('c6', sql.VarChar(40), game.awayName)
+            .input('c7', sql.VarChar(40), game.status)
+            .input('c8', sql.VarChar(40), game.seasonName)
+            .input('c9', sql.Int(4), game.scoreHome)
+            .input('c10', sql.Int(4), game.scoreAway)
+            .input('c11', sql.Int(4), game.team_home_PEN_goals)
+            .input('c12', sql.Int(4), game.team_away_PEN_goals)
+            .input('c13', sql.VarChar(40), category)
+            .query('INSERT INTO games (begin_datetime, game_external_id, home_team_id, home_team_name, away_team_id, away_team_name, status, season_name, score_home, score_away, score_home_pen_goals, score_away_pen_goals, category) values(@c1, @c2, @c3, @c4, @c5, @c6, @c7, @c8, @c9, @c10, @c11, @c12, @c13);');
+          
+            if (result !== null) 
+              console.log(result.rowsAffected);
+        }
+      }
     }
