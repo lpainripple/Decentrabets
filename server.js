4f4fe25a100bc48da4eaee8078ad4b45a3df73f1
Tiago Leite <tleite@ripple.com>
2022-05-10 09:38:16 -0300

    login info

--- server.js
+++ server.js
@@ -60,14 +60,14 @@ app.get("/login", function (request, response) {
 app.post("/login", function (request, response) {
   let username = request.body.username.toLowerCase();
   let password = request.body.password;
-
+  
   console.log(`attempting to login for user: ${username}...`);
 
   if (username && password) {
     console.log(`Input - username: ${username} / password: ${password}`);
     console.log(`running query...`);
     pool.query(
-      "SELECT LOWER(user_name), pass FROM users where user_name = $1 and pass = $2",
+      "SELECT LOWER(user_name), pass, balance FROM users where user_name = $1 and pass = $2",
       [username, password],
       (error, results) => {
         if (error) {
@@ -80,6 +80,7 @@ app.post("/login", function (request, response) {
           //Authentication
           request.session.loggedin = true;
           request.session.username = username;
+          request.session.balance = results.rows[0].balance;
           console.log(
             `session initialized for user ${username}... redirecting to home page`
           );
@@ -200,6 +201,7 @@ app.get("/home", function (request, response) {
 
       response.render("games.ejs", {
         user: request.session.username,
+        balance: request.session.balance,
         games: games,
       });
     }
